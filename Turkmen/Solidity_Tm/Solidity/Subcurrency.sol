61f724412ca1d6fadf24abc024fdfd16a0d37051
Dayanch Muhammetgeldiyev <66455000+DayanchMuhammetgeldiyev@users.noreply.github.com>
2022-09-21 12:04:04 -0700

    Update Subcurrency.sol

--- Turkmen/Solidity_Tm/Solidity/Subcurrency.sol
+++ Turkmen/Solidity_Tm/Solidity/Subcurrency.sol
@@ -2,35 +2,34 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Coin {
-    // "public" anahtar kelimesi değişkenler yapar.
-    // diğer sözleşmelerden erişilebilir
+    // "public" açar söz üýtgeýjileri döredýär.
+    // beýleki şertnamalardan alyp bolýar.
     address public minter;
     mapping (address => uint) public balances;
 
-    // Events, müşterilerin ayrıntılara tepki vermesini sağlar
-    // beyan ettiğiniz sözleşme değişiklikleri
+    // Events, müşderilere jikme-jikliklere reaksiýa bildirmäge mümkinçilik berýär.
+    // yglan edýän şertnama üýtgetmeler.
     event Sent(address from, address to, uint amount);
 
-    // Yapıcı kodu yalnızca sözleşme yapıldığında çalıştırılır.
-    // ve oluşturulur
+    // Konstruktor kody diňe şertnama baglaşylanda işleýär 
+    // we döredilyar
     constructor() {
         minter = msg.sender;
     }
 
-    // Bir adrese yeni oluşturulan bir miktar para gönderir
-    // Yalnızca sözleşmeyi oluşturan kişi tarafından çağrılabilir
+    // Täze döredilen mukdarda adrese pul iberýär.
+    // Diňe şertnamany döreden adam çagyryp biler.
     function mint(address receiver, uint amount) public {
         require(msg.sender == minter);
         balances[receiver] += amount;
     }
 
-    // Hatalar hakkında bilgi vermenizi sağlar
-    // neden bir işlem başarısız oldu. iade edildiler
-    // fonksiyonun arayanına.
+    // Yalnyşlar barada maglumat bermäge mümkinçilik berýär
+    // operasiýa näme üçin şowsuz boldy. Yzyna gaýtaryldy
+    // funksiýany çagyrýan adama.
     error InsufficientBalance ( uint requested, uint available);
 
-    // Mevcut bir miktar para gönderir
-    // herhangi bir arayandan bir adrese
+    // Bar bolan mukdarda pul iberýär.
     function send(address receiver, uint amount) public {
         if (amount > balances[msg.sender])
             revert InsufficientBalance({
